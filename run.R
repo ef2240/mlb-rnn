6f798df9628c85fe36436041bbc3a7d0cf01809c
ef2240 <efeder@2u.com>
2018-02-27 12:55:50 -0500

    separate into functions, filter to position players

--- run.R
+++ run.R
@@ -2,16 +2,34 @@
 library(dplyr)
 library(Lahman)
 
-# Prepare Data
+# Define players in sample (position players debuting since 1988)
+eligible_players <- Appearances %>%
+  inner_join(Master, by = "playerID") %>%
+  group_by(playerID) %>%
+  filter(sum(G_p) / sum(G_all) < 0.5 & debut >= "1988-01-01") %>%
+  distinct(playerID) %>%
+  unlist()
+
+# Conform to one row per season
 season_stats <- Batting %>%
   select(-stint) %>%
   group_by(playerID, yearID) %>%
   summarize_if(is.numeric, sum) %>%
   ungroup()
-calculateSeasonAge = function(birth_month, season_year, birth_year){
+
+# Filter to eligible seasons
+eligible_seasons <- season_stats %>%
+  filter(playerID %in% eligible_players)
+
+# Engineer Features
+engineerFeatures <- function(seasons, master = Master){
+  seasons %>%
+    inner_join(master, by = "playerID") %>%
+    mutate(age = calculateSeasonAge(birthMonth,yearID, birthYear),
+           PA = AB + BB + HBP + SH + SF)
+}
+calculateSeasonAge <- function(birth_month, season_year, birth_year){
   ifelse(birth_month <= 6, season_year - birth_year, season_year - birth_year - 1)
 }
-season_stats %>%
-  inner_join(Master, by = "playerID") %>%
-  filter(debut >= "1988-01-01") %>%
-  mutate(age = calculateSeasonAge(birthMonth,yearID, birthYear))
+
+engineerFeatures(eligible_seasons)
